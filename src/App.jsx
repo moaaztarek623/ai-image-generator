40b1307683715febbdf6860606ad7e65a1de0075
moaaztarek623 <moaaztarek623@gmail.com>
2024-01-18 21:33:19 +0200

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { OpenAI } from 'openai'
 import { v4 as uuidv4 } from 'uuid';
 import FileSaver, { saveAs, FileSaverOptions } from 'file-saver'
+import { BrowserRouter } from 'react-router-dom'
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_APP_API_KEY,
@@ -36,45 +37,47 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div>
-        <h1>Ai Image Generator</h1>
-        <div className="generateContainer" style={{
-          display: 'flex',
-        }}>
-          <input onChange={(e) => setPrompt(e.target.value)} placeholder="Enter your image description" type="text"  />
-          <button onClick={() => fetchData()} style={{
-            borderRadius: '0px',
-          }}>Generate</button>
+    <BrowserRouter basename="ai-image-generator">
+      <div className="App">
+        <div>
+          <h1>Ai Image Generator</h1>
+          <div className="generateContainer" style={{
+            display: 'flex',
+          }}>
+            <input onChange={(e) => setPrompt(e.target.value)} placeholder="Enter your image description" type="text"  />
+            <button onClick={() => fetchData()} style={{
+              borderRadius: '0px',
+            }}>Generate</button>
+          </div>
         </div>
-      </div>
-      {
-        press ? loading ? 
-          <div>
-            <p>Loading ...</p>
-            <p>Please wait until your image ready! </p>
-          </div> 
-        : 
-            <div className="imageCover" style={{marginTop: '1.5rem', border: '2px solid rgba(255, 255, 255)', display: 'flex'}}>
-              <img style={{maxWidth: '100%', maxHeight: '100%',}} alt={prompt} src={image}></img>
-            </div>
+        {
+          press ? loading ? 
+            <div>
+              <p>Loading ...</p>
+              <p>Please wait until your image ready! </p>
+            </div> 
+          : 
+              <div className="imageCover" style={{marginTop: '1.5rem', border: '2px solid rgba(255, 255, 255)', display: 'flex'}}>
+                <img style={{maxWidth: '100%', maxHeight: '100%',}} alt={prompt} src={image}></img>
+              </div>
 
-        : null
-          
-      }
-      <footer style={{
-        position: 'absolute',
-        bottom: '3rem',
-        transform: 'translate(-50%, -50%)',
-        width: '100%',
-        left: '50%'
-      }}>
-      © {new Date().getFullYear()} Copyright: 
-        <a style={{paddingLeft: '0.3rem'}} target="_blank" href='https://www.linkedin.com/in/moaaz-tarek-93304a263/'>
-           Moaaz Tarek
-        </a>
-      </footer>
-    </div>
+          : null
+            
+        }
+        <footer style={{
+          position: 'absolute',
+          bottom: '3rem',
+          transform: 'translate(-50%, -50%)',
+          width: '100%',
+          left: '50%'
+        }}>
+        © {new Date().getFullYear()} Copyright: 
+          <a style={{paddingLeft: '0.3rem'}} target="_blank" href='https://www.linkedin.com/in/moaaz-tarek-93304a263/'>
+            Moaaz Tarek
+          </a>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
