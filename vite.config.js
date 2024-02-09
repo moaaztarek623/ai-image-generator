f29f43560121ab309dfe7ceecd6250403f12270c
moaaztarek623 <moaaztarek623@gmail.com>
2024-02-09 14:24:48 +0200

    solution deploying

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ai-image-generator/'
+  base: 'https://moaaztarek623.github.io/ai-image-generator/'
 })
